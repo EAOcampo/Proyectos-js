44422b3d4b50b74278a501634d2c5e72f0ec4785
Adrian <eaocampo_96@hotmail.com>
2024-04-19 16:44:50 -0600

    first commit

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,10 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+//!Seleccionar boton
+const btn = document.querySelector("#btn")
+
+
+console.log(btn);
+btn.addEventListener("click", ()=>{
+    console.log('Hola');
+})
+
