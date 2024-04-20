56b9bb6da01baded6b5060538de6cc57af50159d
Adrian <eaocampo_96@hotmail.com>
2024-04-19 18:25:01 -0600

    Primer proyecto hecho

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,10 +1,16 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025","pink","grey","brown"];
 //!Seleccionar boton
-const btn = document.querySelector("#btn")
+const btn = document.querySelector("#btn");
+const color = document.querySelector(".color")
+
+// console.log(btn,color);
+btn.addEventListener("click", () => {
+  const random = Math.floor(Math.random() * colors.length);  
+  
+  color.innerHTML = colors[random]
+  document.body.style.backgroundColor = colors[random];
 
 
-console.log(btn);
-btn.addEventListener("click", ()=>{
-    console.log('Hola');
-})
 
+
+});
--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,18 @@
+const btn = document.querySelector("#btn");
+const color = document.querySelector(".color");
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+btn.addEventListener("click", () => {
+    const arrColor = []
+    for (let i = 0; i < 6; i++) {
+        const random = Math.floor(Math.random() * hex.length);
+        arrColor.push(hex[random]);
+    }
+    let cadena = arrColor.join("");
+        
+
+  color.innerHTML = `#${cadena}`
+  document.body.style.backgroundColor = `#${cadena}`;
+});
+
+
